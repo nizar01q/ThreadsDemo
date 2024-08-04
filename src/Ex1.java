d91370506ca03b01c5f6c2925a15c7fa4caab6e5
Asus <nizar01qadoos@gmail.com>
2024-08-04 14:46:02 +0300

    demo of threads

---
+++ src/Ex1.java
@@ -0,0 +1,34 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        Runnable thr1 = () -> {
+            String thrName1 = Thread.currentThread().getName();
+            for (int i = 1; i <= 6; i++) {
+                System.out.println(i+":"+ thrName1);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        Runnable thr2 = () -> {
+            String thrName2 = Thread.currentThread().getName();
+            for (int i = 1; i <= 9; i++) {
+                System.out.println(i+":"+ thrName2);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        Thread t1 = new Thread(thr1,"First Thread");
+        Thread t2 = new Thread(thr2,"Second Thread");
+
+        t1.start();
+        t2.start();
+    }
+}
---
+++ src/Ex2.java
@@ -0,0 +1,23 @@
+public class Ex2  {
+    public static void main(String[] args) {
+        Runnable thr1 = () ->{
+          while (true){
+              try {
+                  Thread.sleep(1000);
+              } catch (InterruptedException e) {
+                  throw new RuntimeException(e);
+              }
+              System.out.println("Running");
+          }
+        };
+        Thread t1 = new Thread(thr1);
+        t1.setDaemon(true);
+        t1.start();
+
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
